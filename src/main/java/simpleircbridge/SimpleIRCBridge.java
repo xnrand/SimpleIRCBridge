aa7d2b581c80fcedc62879e97d0136fc6055dadc
xnrand <xnrand@gmail.com>
2018-01-10 23:21:35 +0100

    Version 1.9.4_1.1.1-dev
    backported to Minecraft 1.9.4

--- src/main/java/simpleircbridge/SimpleIRCBridge.java
+++ src/main/java/simpleircbridge/SimpleIRCBridge.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
 @Mod(modid = SimpleIRCBridge.MODID, version = SimpleIRCBridge.VERSION, acceptableRemoteVersions = "*")
 public class SimpleIRCBridge {
 	public static final String MODID = "simpleircbridge";
-	public static final String VERSION = "1.10.2_1.1.1-dev";
+	public static final String VERSION = "1.9.4_1.1.1-dev";
 
 	private static Logger logger = LogManager.getLogger();
 	private SIBConfig sibConf;
