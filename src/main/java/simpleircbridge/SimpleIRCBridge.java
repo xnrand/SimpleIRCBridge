ec625438f50432119e1b8eb67feda9827e52b53b
xnrand <xnrand@gmail.com>
2018-01-10 22:28:04 +0100

    Version 1.12.2_1.1.1-dev
    new version numbering scheme:
    <mcversion>_<modversion>-<dev/release>

--- src/main/java/simpleircbridge/SimpleIRCBridge.java
+++ src/main/java/simpleircbridge/SimpleIRCBridge.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
 @Mod(modid = SimpleIRCBridge.MODID, version = SimpleIRCBridge.VERSION, acceptableRemoteVersions = "*")
 public class SimpleIRCBridge {
 	public static final String MODID = "simpleircbridge";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.12.2_1.1.1-dev";
 
 	private static Logger logger = LogManager.getLogger();
 	private SIBConfig sibConf;
