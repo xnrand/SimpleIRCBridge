65df5b62783f3e2aa1c3fbbd6fc863692a19d6cc
Christian (Fuchs) Loosli <git@fuchsnet.ch>
2018-01-13 20:11:06 +0100

    Add dependencies for colour support

---
+++ src/main/java/com/michealharker/saraswati/irc/MircColors.java
@@ -0,0 +1,38 @@
+package com.michealharker.saraswati.irc;
+
+/**
+ * Original by michealharker, modified to incldude a bit more
+ * @author Fuchs
+ *
+ */
+public final class MircColors {
+	private MircColors() {
+	}
+
+	public static final String WHITE = "\u000300";
+	public static final String BLACK = "\u000301";
+	public static final String BLUE = "\u000302";
+	public static final String GREEN = "\u000303";
+	public static final String LIGHT_RED = "\u000304";
+	public static final String BROWN = "\u000305";
+	public static final String PURPLE = "\u000306";
+	public static final String ORANGE = "\u000307";
+	public static final String YELLOW = "\u000308";
+	public static final String LIGHT_GREEN = "\u000309";
+	public static final String CYAN = "\u000310";
+	public static final String LIGHT_CYAN = "\u000311";
+	public static final String LIGHT_BLUE = "\u000312";
+	public static final String PINK = "\u000313";
+	public static final String GRAY = "\u000314";
+	public static final String LIGHT_GRAY = "\u000315";
+
+	public static final String TRANSPARENT_BG = "\u000399";
+
+	public static final String BOLD = "\u0002";
+	public static final String NORMAL = "\u000f";
+	public static final String UNDERLINE = "\u001f";
+	
+	public static final String ITALIC = "\u001d";
+	public static final String INVERSE = "\u0016";
+	
+}
