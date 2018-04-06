55c5efa2f0c10abc0e75c1bb0e65644bd2f3e1cf
xnrand <xnrand@gmail.com>
2018-04-06 19:45:11 +0200

    Version 1.7.10_1.1.1
    release

--- src/main/java/simpleircbridge/SimpleIRCBridge.java
+++ src/main/java/simpleircbridge/SimpleIRCBridge.java
@@ -19,7 +19,7 @@ import net.minecraftforge.common.config.Configuration;
 @Mod(modid = SimpleIRCBridge.MODID, version = SimpleIRCBridge.VERSION, acceptableRemoteVersions = "*")
 public class SimpleIRCBridge {
 	public static final String MODID = "simpleircbridge";
-	public static final String VERSION = "1.7.10_1.1.1-dev";
+	public static final String VERSION = "1.7.10_1.1.1";
 
 	private static Logger logger = LogManager.getLogger();
 	private SIBConfig sibConf;
