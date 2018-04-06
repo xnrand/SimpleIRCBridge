83aa268640fcc3d04eb711b5295151ae5d4c59a8
xnrand <xnrand@gmail.com>
2018-04-06 19:41:33 +0200

    Version 1.10.2_1.1.1
    release

--- src/main/java/simpleircbridge/SimpleIRCBridge.java
+++ src/main/java/simpleircbridge/SimpleIRCBridge.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
 @Mod(modid = SimpleIRCBridge.MODID, version = SimpleIRCBridge.VERSION, acceptableRemoteVersions = "*")
 public class SimpleIRCBridge {
 	public static final String MODID = "simpleircbridge";
-	public static final String VERSION = "1.10.2_1.1.1-dev";
+	public static final String VERSION = "1.10.2_1.1.1";
 
 	private static Logger logger = LogManager.getLogger();
 	private SIBConfig sibConf;
