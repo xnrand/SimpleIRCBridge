1140b4bd98e5f15f06cf68a966bf72389c60fc1a
xnrand <xnrand@gmail.com>
2021-01-06 14:16:02 +0100

    Set development version

--- src/main/java/simpleircbridge/SimpleIRCBridge.java
+++ src/main/java/simpleircbridge/SimpleIRCBridge.java
@@ -27,7 +27,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(value = SimpleIRCBridge.MODID)
 public class SimpleIRCBridge {
 	public static final String MODID = "simpleircbridge";
-	public static final String VERSION = "1.16.4_1.2.1";
+	public static final String VERSION = "1.16.4_1.2.2-dev";
 	private static final UUID IRC_UUID = UUID.nameUUIDFromBytes(MODID.getBytes(StandardCharsets.US_ASCII));
 
 	private static Logger logger = LogManager.getLogger(MODID);
